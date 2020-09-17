e01bac1951cba713b59274f1cf2a40df29260f58
kristiyanpetrov <chrustuanpetrow@gmail.com>
2020-09-17 21:03:06 +0100

    flexible lists

--- src/App.js
+++ src/App.js
@@ -5,34 +5,43 @@ import Person from './Person/Person'
 class App extends Component {
   state = {
     persons: [
-      {name: 'Chris', age: 26},
-      {name: 'Susan', age: 29},
-      {name: 'Catherine', age: 23},
+      {id: 'asda', name: 'Chris', age: 26},
+      {id: 'dsdd', name: 'Susan', age: 29},
+      {id: 'aaa', name: 'Catherine', age: 23},
     ],
     otherState: 'some other value',
     showPersons: false
   };
 
-  switchNameHandler = (newName) => {
-    // console.log('Was clicked!')
-    // Don't do this! this.state.persons[0].name = 'Kristiyan';
-    this.setState({
-      persons: [
-        {id: 'dsa', name: 'Kristiyan', age: 26},
-        {id: 'asdads', name: 'Susan', age: 29},
-        {id: 'asdaswq', name: 'Catherine', age: 28}
-      ]
-    });
-  };
+  // switchNameHandler = (newName) => {
+  //   // console.log('Was clicked!')
+  //   // Don't do this! this.state.persons[0].name = 'Kristiyan';
+  //   this.setState({
+  //     persons: [
+  //       {name: 'Kristiyan', age: 26},
+  //       {name: 'Susan', age: 29},
+  //       {name: 'Catherine', age: 28}
+  //     ]
+  //   });
+  // };
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        {name: 'Kristiyan', age: 26},
-        {name: event.target.value, age: 29},
-        {name: 'Catherine', age: 28}
-      ]
-    })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    // const persons = Obejct.assign({}, this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons});
   };
 
   deletePersonHandler = (personIndex) => {
@@ -66,7 +75,9 @@ class App extends Component {
                 click={() => this.deletePersonHandler(index)}
                 name={person.name}
                 age={person.age}
-                key={person.id}/>
+                key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
+             />
            })}
          </div>
       );
